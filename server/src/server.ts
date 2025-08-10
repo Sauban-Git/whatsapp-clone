5e18d3392c0291dded65a1b49404c3b73df6948f
Abu Sauban <sauban.no-reply@gmail.com>
2025-08-10 19:24:22 +0530

    added cors rule

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"
 import { conversationRouter } from "./routes/conversations.js";
 import { messagesRouter } from "./routes/messages.js";
 import { webhookRouter } from "./routes/webHook.js";
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: ["http://localhost:5173", "http://192.168.31.54:5173","http://192.168.31.55:5173"]
+}))
 
 app.use("/conversations", conversationRouter);
 app.use("/messages", messagesRouter);
 app.use("/webhook", webhookRouter)
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server started at port: ${port}`);
 });
