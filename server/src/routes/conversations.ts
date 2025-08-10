17250f5e80d9d92f7bb8d93f25b794209c51eae2
Abu Sauban <sauban.no-reply@gmail.com>
2025-08-10 12:52:44 +0530

    added routers to app

--- server/src/routes/conversations.ts
+++ server/src/routes/conversations.ts
@@ -2,9 +2,17 @@ import { Router, type Request, type Response } from "express";
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response) => {});
-router.post("/", (req: Request, res: Response) => {});
-router.put("/:id", (req: Request, res: Response) => {});
-router.delete("/:id", (req: Request, res: Response) => {});
+router.get("/", (req: Request, res: Response) => {
+    // Get all conversations
+});
+router.post("/", (req: Request, res: Response) => {
+    // Create a conversation
+});
+router.put("/:id", (req: Request, res: Response) => {
+   // Update a conversation 
+});
+router.delete("/:id", (req: Request, res: Response) => {
+    // Delete a conversation
+});
 
 export { router as conversationRouter };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,18 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import { conversationRouter } from "./routes/conversations.js";
+import { messagesRouter } from "./routes/messages.js";
 
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+
+app.use("/conversations", conversationRouter);
+app.use("/messages", messagesRouter);
 
 app.listen(port, () => {
-    console.log(`Server started at port: ${port}`)
-})
+  console.log(`Server started at port: ${port}`);
+});
