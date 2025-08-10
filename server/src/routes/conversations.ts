a9aba4b86e3250c6385f9bf85626d2e3b575eaf7
Abu Sauban <sauban.no-reply@gmail.com>
2025-08-10 14:14:52 +0530

    updated get conversations and added webHook router

--- server/src/routes/conversations.ts
+++ server/src/routes/conversations.ts
@@ -1,18 +1,61 @@
 import { Router, type Request, type Response } from "express";
+import { PrismaClient } from "../generated/prisma/index.js";
 
+const prisma = new PrismaClient();
 const router = Router();
 
-router.get("/", (req: Request, res: Response) => {
-    // Get all conversations
+router.get("/", async (req: Request, res: Response) => {
+  const body = req.body;
+  try {
+    const conversations = await prisma.conversation.findMany({
+      where: {
+        participants: {
+          some: {
+            userId: body.userId,
+          },
+        },
+      },
+      select: {
+        id: true,
+        name: true,
+        isGroup: true,
+        createdAt: true,
+        Message: {
+          orderBy: {
+            createdAt: "desc",
+          },
+          take: 1,
+          select: {
+            content: true, 
+            createdAt: true, 
+            senderId: true, 
+            type: true, 
+          },
+        },
+      },
+    });
+
+    return res.status(200).json({
+      conversations,
+    });
+  } catch (error) {
+    console.error("Error fetching conversations: ", error);
+    return res.status(500).json({
+      error: "Failed to get conversations",
+    });
+  }
 });
-router.post("/", (req: Request, res: Response) => {
-    // Create a conversation
+
+router.post("/", async (req: Request, res: Response) => {
+  // Create a conversation
 });
-router.put("/:id", (req: Request, res: Response) => {
-   // Update a conversation 
+
+router.put("/:id", async (req: Request, res: Response) => {
+  // Update a conversation
 });
-router.delete("/:id", (req: Request, res: Response) => {
-    // Delete a conversation
+
+router.delete("/:id", async (req: Request, res: Response) => {
+  // Delete a conversation
 });
 
 export { router as conversationRouter };
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { conversationRouter } from "./routes/conversations.js";
 import { messagesRouter } from "./routes/messages.js";
+import { webhookRouter } from "./routes/webHook.js";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use("/conversations", conversationRouter);
 app.use("/messages", messagesRouter);
+app.use("/webhook", webhookRouter)
 
 app.listen(port, () => {
   console.log(`Server started at port: ${port}`);
