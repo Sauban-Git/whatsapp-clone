ab762ff97751106312afd3d33c845a8f055d5aca
Abu Sauban <sauban.no-reply@gmail.com>
2025-08-16 09:19:56 +0530

    Added green tint and updated loading message

--- client/src/components/LoadingScreen.tsx
+++ client/src/components/LoadingScreen.tsx
@@ -3,13 +3,13 @@ export const LoadingScreen = () => {
     <div className="flex justify-center w-screen h-screen items-center">
       <div className="rounded-xl bg-gray-200 p-3 shadow-md backdrop-blur-sm">
         <div className="text-center">
-          <p>Please wait.</p>
-          <p>This may take a while as server is booting...</p>
+          <p>Waking up the server...</p>
+          <p>It had a long nap. Almost there!</p>
         </div>
         <div className="flex justify-center flex-row gap-2 m-3">
-          <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:0s]"></div>
-          <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:0.1s]"></div>
-          <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:0.2s]"></div>
+          <div className="w-4 h-4 rounded-full bg-[#25D350] animate-bounce [animation-delay:0s]"></div>
+          <div className="w-4 h-4 rounded-full bg-[#25D350] animate-bounce [animation-delay:0.1s]"></div>
+          <div className="w-4 h-4 rounded-full bg-[#25D350] animate-bounce [animation-delay:0.2s]"></div>
         </div>
       </div>
     </div>
